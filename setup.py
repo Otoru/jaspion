60e190a7511e3d43b21336be722b2497bd26d395
Vitor Hugo <vitor.hov@gmail.com>
2019-06-05 07:45:18 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         'Operating System :: Unix',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Communications :: Telephony',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Application Frameworks',
