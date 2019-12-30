4e14b304b4b0850c5eca83ad6fa32f8244bccf9d
Vitor Hugo <vitor.hov@gmail.com>
2019-12-30 14:39:17 -0300

    Remove sup to python 3.5 and add sup to python 3.8

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         "Intended Audience :: Telecommunications Industry",
         "Natural Language :: Portuguese (Brazilian)",
         "Operating System :: Unix",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
